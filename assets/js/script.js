42937b45f80750316759fb2dfcfd08ddc215623c
RamirezJM <165516814+RamirezJM@users.noreply.github.com>
2025-08-28 23:09:37 -0400

    first upload

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+const menuButton = document.querySelector('.hamburger-menu')
+const navMenu = document.querySelector('nav')
+
+menuButton.addEventListener('click', () => {
+  const isActive = menuButton.getAttribute('aria-expanded') === 'true';
+  menuButton.setAttribute('aria-expanded', String(!isActive))
+  navMenu.classList.toggle('active')
+})
+
+
+
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  autoplay: {
+    delay: 5000,
+  },
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
+
